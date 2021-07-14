760cc6a50c3040a50c58397c4b97c8bb59758c69
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-07-14 16:09:16 +0900

    sidebar component

---
+++ client/src/components/SideBar.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { Tab, Nav, Button, Modal } from 'react-bootstrap'
+
+
+
+export const SideBar = (id) => {
+    const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
+
+    return (
+    <div style={{ width: '250px' }} className="d-flex flex-column">
+      <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
+        <Nav variant="tabs" className="justify-content-center">
+          <Nav.Item>
+            <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Tab.Content className="border-right overflow-auto flex-grow-1">
+          <Tab.Pane eventKey={CONVERSATIONS_KEY}>
+            <Conversations />
+          </Tab.Pane>
+          <Tab.Pane eventKey={CONTACTS_KEY}>
+            <Contacts />
+          </Tab.Pane>
+        </Tab.Content>
+        <div className="p-2 border-top border-right small">
+          Your Id: <span className="text-muted">{id}</span>
+        </div>
+        <Button onClick={() => setModalOpen(true)} className="rounded-0">
+          New {conversationsOpen ? 'Conversation' : 'Contact'}
+        </Button>
+      </Tab.Container>
+    )
+}
