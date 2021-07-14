86976f66ae8001cd6178806f307c11e202556537
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-07-14 15:45:27 +0900

    login logic

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,14 @@
+import React from 'react'
 import Login from './Login'
+import useLocalStorage from '../hooks/useLocalStorage';
 
 function App() {
+  const [id, setId] = useLocalStorage('id')
+
+
   return (
-    <Login />
-  );
+    id ? dashboard : <Login onIdSubmit={setId} />
+  )
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { v4 as uuidV4 } from 'uuid'
 
 ReactDOM.render(
   <React.StrictMode>
