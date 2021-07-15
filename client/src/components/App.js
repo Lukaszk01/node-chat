180a3c17f09d49faf491d3f90698091353611a5d
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-07-15 19:00:08 +0900

    contacts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,16 @@ import useLocalStorage from '../hooks/useLocalStorage';
 
 function App() {
   const [id, setId] = useLocalStorage('id')
-
+  
+  const dashboard = (
+    <SocketProvider id={id}>
+      <ContactsProvider>
+        <ConversationsProvider id={id}>
+          <Dashboard id={id} />
+        </ConversationsProvider>
+      </ContactsProvider>
+    </SocketProvider>
+  )
 
   return (
     id ? dashboard : <Login onIdSubmit={setId} />
