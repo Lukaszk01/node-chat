d08b266b1e3b8ce46cbc9147065928b64c446bdb
Lukaszk01 <lukaszkkoziel@gmail.com>
2021-07-24 15:11:41 +0900

    login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -14,7 +14,7 @@ export default function Login({ onIdSubmit }) {
   function createNewId() {
     onIdSubmit(uuidV4())
   }
-
+  document.body.style = "linear-gradient(<angle>, color1 color1-stop-at, color2 color2-start-at);";
   return (
     <Container className="align-items-center d-flex" style={{ height: '100vh' }}>
       <Form onSubmit={handleSubmit} className="w-100">
@@ -22,8 +22,8 @@ export default function Login({ onIdSubmit }) {
           <Form.Label>Enter Your Id</Form.Label>
           <Form.Control type="text" ref={idRef} required />
         </Form.Group>   
-        <Button type="submit" className="mr-2 ml-3 btn btn-success" >Login</Button>
-        <Button className="ml-3" onClick={createNewId} variant="success">Create A New Id</Button>
+        <Button type="submit" className="mr-2 ml-3 mt-3 btn btn-success" style={{ marginRight: '5px'}}>Login</Button>
+        <Button className="ml-3 mt-3" onClick={createNewId} variant="success">Create A New Id</Button>
       </Form>
     </Container>
   )
